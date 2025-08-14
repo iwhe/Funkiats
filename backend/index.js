2669c321d60aeecb68307e34a72049ea2e08b716
Bhupesh <bhupeshpaneru7@gmail.com>
2025-08-15 03:42:29 +0545

    included 2nd frontend url to cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
